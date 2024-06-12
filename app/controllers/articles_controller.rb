d185557181e9f91f6e441dc68145d22f5c87ac72
Pedro <pedro.costa@rakuten.com>
2024-06-12 09:34:41 +0900

    added edit / update to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
   
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -19,6 +23,16 @@ class ArticlesController < ApplicationController
       redirect_to @article # shorcut of typing -> article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article has been updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
